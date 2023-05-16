6abf65e2f732e08213bffb271302103e38170a07
Rohit <makawanarohit70@gmail.com>
2023-05-16 15:31:20 +0530

    Firestore and Database

---
+++ App.js
@@ -0,0 +1,41 @@
+import {View, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+// import firestore from '@react-native-firebase/firestore';
+import database from '@react-native-firebase/database';
+import Home from './screens/Home';
+const App = () => {
+  useEffect(() => {
+    getDataBase();
+  }, []);
+  const [myData, setMyData] = useState(null);
+  const getDataBase = async () => {
+    try {
+      const data = await database().ref('users/1').once('value');
+      //   console.log(data);
+      setMyData(data.val());
+
+      //This is for FireStore
+      //   const data = await firestore()
+      //     .collection('Testing')
+      //     .doc('cSbOnDpe3KKsKbLlVD3p')
+      //     .get();
+      //   setMyData(data._data);
+      //   console.log(data._data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <View className="mt-8 px-5  bg-slate-500 py-2">
+      <Text className="text-red-600">
+        Name: {myData ? myData.Name : 'Loging...'}
+      </Text>
+      <Text className="text-red-900">
+        Age: {myData ? myData.Age : 'Loging...'}
+      </Text>
+      <Text>City: {myData ? myData.City : 'Loging...'}</Text>
+    </View>
+  );
+};
+
+export default App;
