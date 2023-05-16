ec531c5898e0fc8b7f37586a636981c32ff1b6e4
Rohit <makawanarohit70@gmail.com>
2023-05-16 16:16:46 +0530

    Add in fireBase

--- App.js
+++ App.js
@@ -1,41 +1,33 @@
-import {View, Text} from 'react-native';
-import React, {useEffect, useState} from 'react';
-// import firestore from '@react-native-firebase/firestore';
+import {View, TextInput, TouchableOpacity, Text} from 'react-native';
+import React, {useState} from 'react';
 import database from '@react-native-firebase/database';
-import Home from './screens/Home';
-const App = () => {
-  useEffect(() => {
-    getDataBase();
-  }, []);
-  const [myData, setMyData] = useState(null);
-  const getDataBase = async () => {
-    try {
-      const data = await database().ref('users/1').once('value');
-      //   console.log(data);
-      setMyData(data.val());
 
-      //This is for FireStore
-      //   const data = await firestore()
-      //     .collection('Testing')
-      //     .doc('cSbOnDpe3KKsKbLlVD3p')
-      //     .get();
-      //   setMyData(data._data);
-      //   console.log(data._data);
+const App = () => {
+  const [input, setInput] = useState(null);
+  const [list, setList] = useState(null);
+  const handleData = async () => {
+    try {
+      const response = await database().ref('todo/2').set({value: input});
+      console.log(response);
     } catch (error) {
       console.log(error);
     }
   };
   return (
-    <View className="mt-8 px-5  bg-slate-500 py-2 items-center">
-      <Text className="text-black font-bold text-xl">
-        Name: {myData ? myData.Name : 'Loging...'}
-      </Text>
-      <Text className="text-black font-bold text-xl">
-        Age: {myData ? myData.Age : 'Loging...'}
-      </Text>
-      <Text className="text-black font-bold text-xl">
-        City: {myData ? myData.City : 'Loging...'}
-      </Text>
+    <View className="flex-1 bg-white">
+      <View className="justify-center items-center m-3">
+        <TextInput
+          className="w-full border-solid border-black mt-10 px-5 border-2 rounded-sm"
+          placeholder="Enter Your Name"
+          onChangeText={value => setInput(value)}
+          value={input}
+        />
+        <TouchableOpacity
+          className="bg-blue-500 w-full mx-3 my-5 py-2 rounded-md"
+          onPress={() => handleData()}>
+          <Text className="text-center text-xl font-bold text-black">Add</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
